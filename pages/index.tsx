dda1c2a7e25fb35d18303fe95ef90d71fd8faae1
Ade Rohayat <muhamadaderohayat122@gmail.com>
2021-11-28 17:29:40 +0700

    Optimalisasi Image di NextJS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <Layout pageTitle="Home Page">
+      <Image src="/icon-user.png" width={300} height={300} alt="user"></Image>
       <h1 className={styles["title-homepage"]}>Hello Ade</h1>
     </Layout>
   );
