e910a0d9409f57a35a5ec58f5037b2232ede91d9
Ade Rohayat <muhamadaderohayat122@gmail.com>
2021-11-28 17:10:15 +0700

    Auto redirection di NextJS

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,12 @@
-import Layout from "../components/Layout";
+import { useEffect } from "react";
+import router, { useRouter } from "next/router";
 
 export default function page404() {
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 2000);
+  }, []);
   return (
     <div>
       <h1>Oooopss....</h1>
