67c9e45a5bcb91255539e33dcb4328a82db4dcda
Ade Rohayat <muhamadaderohayat122@gmail.com>
2021-11-28 17:02:16 +0700

    Custom halaman 404 (Not Found Page)

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function page404() {
+  return (
+    <div>
+      <h1>Oooopss....</h1>
+      <h4>Halaman yang anda cari tidak ditemukan</h4>
+    </div>
+  );
+}
