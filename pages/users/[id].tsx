ec1b079c265ab076cf0761a55e751a10a89f6bdc
Ade Rohayat <muhamadaderohayat122@gmail.com>
2021-12-05 10:28:11 +0700

    Data Fetching getStaticPaths

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,15 +1,75 @@
-import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 
-export default function Blog() {
-  const router = useRouter();
-  const { id } = router.query;
+interface User {
+  id: number;
+}
+interface UsersDetailProps {
+  user: any;
+}
+
+export default function UsersDetail(props: UsersDetailProps) {
+  const { user } = props;
   return (
-    <Layout pageTitle="Blog Page">
+    <Layout pageTitle="User Page">
       <h1 className="title">
-        Blog
-        {id}
+        {user.name}
       </h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>Email</td>
+            <td>&nbsp;:&nbsp;</td>
+            <td>{user.email}</td>
+          </tr>
+          <tr>
+            <td>Username</td>
+            <td>&nbsp;:&nbsp;</td>
+            <td>{user.username}</td>
+          </tr>
+          <tr>
+            <td>Phone</td>
+            <td>&nbsp;:&nbsp;</td>
+            <td>{user.phone}</td>
+          </tr>
+          <tr>
+            <td>Website</td>
+            <td>&nbsp;:&nbsp;</td>
+            <td>{user.website}</td>
+          </tr>
+        </tbody>
+      </table>
     </Layout>
   );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const dataUsers = await res.json();
+  const paths = dataUsers.map((user: User) => ({
+    params: {
+      id: `${user.id}`,
+    },
+  }));
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+interface GetStaticProps {
+  params: {
+    id: any;
+  }
+}
+
+export async function getStaticProps(context: GetStaticProps) {
+  const { id } = context.params;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const user = await res.json();
+
+  return {
+    props: {
+      user,
+    },
+  }
 }
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,33 +1,33 @@
-import Link from "next/link";
+import { useRouter } from 'next/router';
 import Layout from "../../components/Layout";
+import styles from '../../styles/Users.module.css';
 
 interface UserProps {
   dataUsers: Array<any>;
 }
 
-export default function Blog(props: UserProps) {
+export default function User(props: UserProps) {
   const { dataUsers } = props;
-  console.log(dataUsers);
+  const router = useRouter();
+
   return (
-    <Layout pageTitle="Blog Page">
+    <Layout pageTitle="Users Page">
       <h1 className="title">Users</h1>
-      <ul>
-        {dataUsers.map((user) => (
-          <li key={user.id}>
-            <span>
-              {user.name}
-              &nbsp;&nbsp;&nbsp;
-            </span>
-            <Link href="/users/{user.id}">
-              <span>
-                mailto:
-                {user.email}
-              </span>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </Layout>
+      {dataUsers.map((user) => (
+        <div key={user.id} onClick={() => router.push(`/users/${user.id}`)} className={styles.card}>
+          <span>
+            {user.name}
+            &nbsp;&nbsp;&nbsp;
+          </span>
+          <br />
+          <span>
+            mailto:
+            {user.email}
+          </span>
+        </div>
+      ))
+      }
+    </Layout >
   );
 }
 
